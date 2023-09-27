c151d3a3cbc28fa1aab0a07dd96eabff95e48953
erdodif <erdodif@gmail.com>
2023-09-27 23:33:33 +0200

    optimize imports

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,88 +1,76 @@
 <script lang="ts">
-    import {
-        Branch,
-        IfStatement,
-        LoopStatement,
-        SimpleStatement,
-        SwitchStatement,
-    } from "$lib/classes/Statement";
     import { StructogramSerializer } from "$lib/classes/StatementSerializer";
-    import Structorgram from "$lib/classes/Structogram";
     import Structogram from "$lib/elements/Structogram.svelte";
     import { StructogramStore } from "$lib/stores/structogram";
 
-    let struk = new StructogramStore(StructogramSerializer.fromJson(
-    {
-        type:"function",
-        signature:"idk man",
-        statements:[
-            {
-                type:"if",
-                condition: "if this is it",
-                block:[
-                    {
-                        type:"loop",
-                        condition:"if true",
-                        block:[
-                           {
-                            type:"simple",
-                            content:"just loopin' around"
-                           } 
-                        ]
-                    }
-                ],
-                elseblock:[
-                    {
-                        type:"skip",
-                        content:"SKIP"
-                    }
-                ]
-            },
-            {
-                type:"loop-reverse",
-                condition:"Are you done yet?",
-                block:[
-                    {
-                        type:"switch",
-                        cases:[
-                            {
-                                condition:"1",
-                                block:[
-                                    {
-                                        type:"simple",
-                                        content:"Yes"
-                                    }
-                                ]
-                            },
-                            {
-                                condition:"2",
-                                block:[
-                                    {
-                                        type:"simple",
-                                        content:"Maybe"
-                                    }
-                                ]
-                            },
-                            {
-                                condition:"3",
-                                block:[
-                                    {
-                                        type:"simple",
-                                        content:"No"
-                                    }
-                                ]
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
-    }
-    ));
+    let struk = new StructogramStore(
+        StructogramSerializer.fromJson({
+            type: "function",
+            signature: "idk man",
+            statements: [
+                {
+                    type: "if",
+                    condition: "if this is it",
+                    block: [
+                        {
+                            type: "loop",
+                            condition: "if true",
+                            block: [
+                                {
+                                    type: "simple",
+                                    content: "just loopin' around",
+                                },
+                            ],
+                        },
+                    ],
+                    elseblock: [
+                        {
+                            type: "skip",
+                            content: "SKIP",
+                        },
+                    ],
+                },
+                {
+                    type: "loop-reverse",
+                    condition: "Are you done yet?",
+                    block: [
+                        {
+                            type: "switch",
+                            cases: [
+                                {
+                                    condition: "1",
+                                    block: [
+                                        {
+                                            type: "simple",
+                                            content: "Yes",
+                                        },
+                                    ],
+                                },
+                                {
+                                    condition: "2",
+                                    block: [
+                                        {
+                                            type: "simple",
+                                            content: "Maybe",
+                                        },
+                                    ],
+                                },
+                                {
+                                    condition: "3",
+                                    block: [
+                                        {
+                                            type: "simple",
+                                            content: "No",
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                },
+            ],
+        })
+    );
 </script>
 
 <Structogram structogram={struk} />
-
-<code>
-    {JSON.stringify(StructogramSerializer.toJson($struk))}
-</code>
