fcb60d998faf9250e62c6e44bc82e3ae7e3dba20
erdodif <erdodif@gmail.com>
2024-01-29 16:00:55 +0100

    new elements for parallel programs

---
+++ src/lib/elements/statements/AwaitElement.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import type { AwaitStatement } from "$lib/classes/Statement";
+    import type { StatementStore } from "$lib/stores/structogram";
+    import CustomTextArea from "../CustomTextArea.svelte";
+
+    export let statement: StatementStore<AwaitStatement>;
+</script>
+
+<div class="await">
+    <CustomTextArea name="guard" bind:value={$statement.content} />
+</div>
+
+<style lang="scss">
+    .await {
+        // TODO arch over the guard
+        padding-inline: $struc-padding-inline;
+        padding-block: $struc-padding-block;
+        box-sizing: border-box;
+        height: 100%;
+        text-align: center;
+    }
+</style>
---
+++ src/lib/elements/statements/ParallelElement.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import type { ParallelStatement } from "$lib/classes/Statement";
+    import type { StatementStore } from "$lib/stores/structogram";
+    import Statement from "../Statement.svelte";
+    //TODO - Make it look parallel
+
+    export let statement: StatementStore<ParallelStatement>;
+</script>
+
+<div class="switch">
+    {#each $statement.threads as threads, i (i)}
+        <div class="thread">
+            {#each threads.map( (stmt) => statement.getStoreOf(stmt) ) as stmt}
+                <Statement statement={stmt} />
+            {/each}
+        </div>
+    {/each}
+</div>
+
+<style lang="scss">
+    //TODO - design with the signature parallel lines between the blocks
+</style>
